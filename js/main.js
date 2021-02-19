56a7d502b285b49418841a50fbfcf91d0c72ceea
Michał Wilk <dev2theworld@gmail.com>
2021-02-19 20:58:48 +0100

    JS - color changing function + transition effect added

--- js/main.js
+++ js/main.js
@@ -4,5 +4,21 @@ for (var i=0; themeDots.length > i; i++){
   themeDots[i].addEventListener('click', function(){
     let mode = this.dataset.mode;
     console.log('Option clicked:', mode);
+    setTheme(mode);
   })
+}
+
+function setTheme(mode){
+  if(mode == 'light'){
+    document.getElementById('theme-style').href = './css/main.css';
+  }
+  if(mode == 'blue'){
+    document.getElementById('theme-style').href = './css/main-blue.css';
+  }
+  if(mode == 'green'){
+    document.getElementById('theme-style').href = './css/main-green.css';
+  }
+  if(mode == 'purple'){
+    document.getElementById('theme-style').href = './css/main-purple.css';
+  }
 }
